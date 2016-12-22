cfdd2debfd72df84f3a12ec18c4ffca75aae9b48
Galaxy <jiaqi0109@gmail.com>
2016-12-22 23:56:39 +0800

    First petch.

---
+++ app/src/main/java/com/example/jiaqi/filesmanagement/MainLogin.java
@@ -0,0 +1,52 @@
+package com.example.jiaqi.filesmanagement;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainLogin extends Activity {
+
+    private Button btup;
+    private Button btin;
+    private TextView textuser;
+    private TextView textpw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+
+        textuser = (TextView) findViewById(R.id.username);
+        textpw = (TextView) findViewById(R.id.pwname);
+        /*
+         *  注册按钮跳转
+         */
+        btup = (Button) findViewById(R.id.sign_up);
+        btup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainLogin.this, SignUp.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+    /*
+     *  接收回传数据
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1&& resultCode == 2){
+            String content = data.getStringExtra("data");
+            textuser.setText(content);
+            Toast.makeText(getApplicationContext(), "Sign-up Success", Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+}
+
---
+++ app/src/main/java/com/example/jiaqi/filesmanagement/SignUp.java
@@ -0,0 +1,51 @@
+package com.example.jiaqi.filesmanagement;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SignUp extends AppCompatActivity {
+
+    private Button btsg;
+    private TextView textuser;
+    private TextView textpw1;
+    private TextView textpw2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        textuser = (TextView) findViewById(R.id.editText1);
+        textpw1 = (TextView) findViewById(R.id.editText2);
+        textpw2 = (TextView) findViewById(R.id.editText3);
+        /*
+         *  按钮click操作
+         */
+        btsg = (Button) findViewById(R.id.return1);
+        btsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textpw2.getText().toString().trim().equals(textpw1.getText().toString().trim())) {
+                    Intent data = new Intent();
+                    data.putExtra("data", textuser.getText().toString());
+                    setResult(2, data);
+                /*
+                 *  结束此页面
+                 */
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Sign-up Fault", Toast.LENGTH_LONG).show();
+                    textpw1.setText("");
+                    textpw2.setText("");
+                }
+            }
+        });
+
+    }
+}
